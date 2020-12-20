811e8318b71202b9f6722cd96a068626a75cdddf
yusrilsabir22 <yusrilsabir22@gmail.com>
2020-12-20 19:32:41 +0700

    first commit

---
+++ database/migrations/2020_09_14_105628_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->string('item_id');
+            $table->primary("item_id");
+            $table->integer('category_id');
+            $table->foreign("category_id")
+                  ->references("id")
+                  ->on("categories");
+            $table->string("name");
+            $table->timestamp("created_at");
+            $table->timestamp("updated_at");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
---
+++ resources/plugin/vuetify.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Vuetify, { VLayout } from 'vuetify/lib'
+
+Vue.use(Vuetify, {
+    components: { VLayout },
+})
+
+const opts = {}
+
+export default new Vuetify(opts)
